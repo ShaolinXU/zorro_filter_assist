fd235ea1d7f4aaed22d1e08aaf669783c68cea0f
ShaolinXU <56570371+ShaolinXU@users.noreply.github.com>
2020-07-01 14:19:38 +0800

    Create zorro_snakemake.py

---
+++ zorro_snakemake.py
@@ -0,0 +1,32 @@
+import os
+
+seq_files = os.listdir("seqs")
+
+filenames_stripped = [os.path.splitext(file)[0] for file in seq_files]
+
+rule all:
+    input:
+        expand("filtered/{seq}_filtered.fasta",seq=filenames_stripped)
+
+rule zorro:
+    input:
+        "seqs/{seq}.fasta"
+    output:
+        "masks/{seq}.mask"
+    threads:
+        1
+    shell:
+        "~/bin/zorro_linux_x86_64 -sample {input} > {output}"
+        
+rule filter:
+    input:
+        seq="seqs/{seq}.fasta",
+        mask="masks/{seq}.mask"
+    output:
+        "filtered/{seq}_filtered.fasta"
+    threads:
+        1
+    shell:
+        """
+        ~/bin/zorro.py -s {input.seq} -m {input.mask} -t 5 -o {output}
+        """
